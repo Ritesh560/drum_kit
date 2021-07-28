b16ba0ce14a5577b2322de205b2d443032c24ac5
Ritesh560 <75880449+Ritesh560@users.noreply.github.com>
2021-07-28 10:36:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+function sound(key)
+    { 
+        switch(key)
+        {
+            
+            case "a":
+                var A = new Audio("1.wav")
+                A.play();"a".addClass("play");
+                break;
+            case "s":
+            var S= new Audio("2.wav");
+            S.play();
+            break;
+            case "d":
+            var D= new Audio("3.wav");
+            D.play();
+            break;case "f":
+            var F= new Audio("4.wav");
+            F.play();
+            break;case "g":
+            var G= new Audio("5.wav");
+            G.play();
+            break;case "h":
+            var H= new Audio("6.wav");
+            H.play();
+            break;
+            case "j":
+            var J= new Audio("7.wav");
+            J.play();
+            break;
+            case "k":
+            var k= new Audio("8.wav");
+            k.play();
+            break;
+            case "l":
+            var L= new Audio("9.wav");
+            L.play();
+            break;
+        }
+    }
+
+   function press(key)
+   {
+       
+      var btnpress= document.querySelector("."+key);
+      btnpress.classList.add("pressed");
+      setTimeout(function(){
+          btnpress.classList.remove("pressed");
+      },100);
+   }
+    
+    noofbuttons= document.querySelectorAll(".button").length; 
+    for(var i=0; i<noofbuttons; i++)
+    {
+       document.querySelectorAll(".button")[i].addEventListener("click", function() {
+           var playonclick = this.innerHTML;
+           sound(playonclick);
+           press(playonclick);
+           
+       });
+    }
+
+    
+    document.addEventListener("keypress",function(p)
+    {
+        press(p.key); 
+        sound(p.key);
+    })
+
+
